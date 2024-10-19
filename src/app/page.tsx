ce4fdc3b09f134af46db2c5a95f4d14e4dfd0b3a
unknown <raihan.dev3@gmail.com>
2024-10-19 17:36:02 +0600

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Header from "./components/Common/Header/Header";
 import HeroSection from "./components/Home/HeroSection/HeroSection";
+import "@/app/styles/home.css";
 
 export default function Home() {
   return (
     <div>
        <Header></Header>
-       <HeroSection></HeroSection>
+       <HeroSection />
     </div>
   );
 }
