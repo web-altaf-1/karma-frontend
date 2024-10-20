92949f48702b99b89e383b4bf5389363ea775ae2
ALTAF <web.altaf.1@gmail.com>
2024-10-20 11:49:37 +0600

    added functional area

---
+++ src/app/components/Home/FunctionalArea/FunctionalArea.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import Icon1 from "@/app/assets/industry/icons8-operation-64.svg";
+import Image from "next/image";
+const FunctionalArea = () => {
+  const data = [
+    {
+      id: 1,
+      icon: Icon1,
+      text: "উৎপাদন/অপারেশন",
+      value: 0,
+    },
+    {
+      id: 2,
+      icon: Icon1,
+      text: "আতিথেয়তা/ভ্রমণ/পর্যটন",
+      value: 0,
+    },
+    {
+      id: 3,
+      icon: Icon1,
+      text: "সৌন্দর্যের যত্ন/স্বাস্থ্য",
+      value: 0,
+    },
+    {
+      id: 4,
+      icon: Icon1,
+      text: "অ্যাকাউন্টিং ও ফাইন্যান্স",
+      value: 0,
+    },
+    {
+      id: 5,
+      icon: Icon1,
+      text: "সেক্রেটারি/রিসেপশনিস্ট",
+      value: 0,
+    },
+    {
+      id: 6,
+      icon: Icon1,
+      text: "শিক্ষা/প্রশিক্ষণ",
+      value: 0,
+    },
+    {
+      id: 7,
+      icon: Icon1,
+      text: "কৃষি (উদ্ভিদ/প্রাণী/মাছ)",
+      value: 0,
+    },
+    {
+      id: 8,
+      icon: Icon1,
+      text: "ইলেকশিয়ান/নির্মাণ",
+      value: 0,
+    },
+  ];
+  return (
+    <div className="functional__area">
+      <div className="main-container">
+        <h3 className="area-title" style={{ marginBottom: "70px" }}>
+          ইন্ডাস্ট্রি অনুযায়ী চাকুরি BY FUNCTIONAL AREA
+        </h3>
+        <div className="functional-items">
+          {data?.map((item, index) => {
+            return (
+              <div key={index} className="functional-item">
+                <Image width={22} height={22} src={item?.icon} alt="icon" />
+                <h4 className="item-text">{item?.text} ({item?.value})</h4>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FunctionalArea;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,17 @@ import CategorySection from "./components/Home/CategorySection";
 import HeroSection from "./components/Home/HeroSection/HeroSection";
 import "@/app/styles/home.css";
 import SearchByIndustry from "./components/Home/SearchByIndustry";
+import FunctionalArea from "./components/Home/FunctionalArea/FunctionalArea";
 
 export default function Home() {
   return (
     <div>
-       <Header></Header>
-       <HeroSection />
-       <CategorySection></CategorySection>
-       <SearchByIndustry></SearchByIndustry>
+      <Header />
+      <HeroSection />
+      <CategorySection />
+      <SearchByIndustry />
+
+      <FunctionalArea />
     </div>
   );
 }
